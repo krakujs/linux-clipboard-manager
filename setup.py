29387585c3de60ae553cfbd385935bec35575dab
ujjwal <ujjwalsolanki@jklu.edu.in>
2025-11-05 10:12:00 +0100

    Bump version to 1.1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if requirements_file.exists():
 
 setup(
     name="linux-clipboard-manager",
-    version="1.1.1",
+    version="1.1.2",
     author="Smart Clipboard Team",
     author_email="contact@smart-clipboard.com",
     description="A powerful clipboard manager with history, search, and smart categorization",
