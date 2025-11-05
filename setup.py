8cf253246e0df9cadf241fc283c0c979e4b2882f
ujjwal <ujjwalsolanki@jklu.edu.in>
2025-11-05 09:41:23 +0100

    Update to version 1.0.6 for fixed publishing workflow

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if requirements_file.exists():
 
 setup(
     name="smart-clipboard-manager",
-    version="1.0.5",
+    version="1.0.6",
     author="Smart Clipboard Team",
     author_email="contact@smart-clipboard.com",
     description="A powerful clipboard manager with history, search, and smart categorization",
